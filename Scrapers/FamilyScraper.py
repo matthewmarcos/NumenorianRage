9e0abaac7d5815d6a53becebd44a482b0987052e
Matthew Marcos <matthewmarcos94@gmail.com>
2020-02-25 22:30:02 +0800

    Initial Cleaning and Data Exploration

--- Scrapers/FamilyScraper.py
+++ Scrapers/FamilyScraper.py
@@ -1,71 +1,170 @@
+from operator import itemgetter
 from scrapy import Request, Spider
-# from scrapy.Selector import HtmlXPathSelector
-import csv
-import json
-import os
-from CharacterParser import CharacterParser
+from csv import DictWriter
+import re
+import unicodedata
 
-"""
-Cool Stuff
-"""
+
 class FamilyScraper(Spider):
     name = 'numenorian_kings_scraper'
-    output_file = 'kings.txt'
+    output_file = 'kings.psv'
     start_urls = [
-        'http://tolkiengateway.net/wiki/Elros',  # Numenorian Kings 
-        # 'http://tolkiengateway.net/wiki/Elendil'  # Royal bloodline of Arnor and Gondor
+        # Numenorian Kings
+        'http://tolkiengateway.net/wiki/Elros',
+        # Royal bloodline of Arnor and Gondor
+        'http://tolkiengateway.net/wiki/Elendil'
     ]
-
-    table_label_mapping = {
-        # Other Names
-        'Other names': 'other_names',
-        # Get Title (If any)
-        'Titles': 'titles',
-        # Birth
-        'Birth': 'birth',
-        # Rule
-        'Rule': 'rule',
-        # Death 
-        'Death': 'death',
-    }
 
     def start_requests(self):
         for url in self.start_urls:
-            yield Request(url=url, callback=self.parse)
+            yield Request(url=url, callback=self.parse, meta={'depth': 1})
+
+    def get_main_table(self, tables_selector):
+        tables = [(table_selector, len(table_selector.css('tr').getall()))
+                  for table_selector in tables_selector]
+
+        # This is not perfect. Doesn't matter, we got all the Numenorian Kings
+        #  and members of the royal bloodline
+        if(len(tables_selector) < 5):
+            return tables_selector[0]
+
+        # We know for certain that the main table is the largest of the
+        # first 2 tables. Plz not dispute. This was a shot in the dark
+        return max(tables[:2], key=itemgetter(1))[0]
 
     def parse(self, response):
         BASE_URL = 'http://tolkiengateway.net'
         table_selector = '#mainContent table'
-
-        table_rows = response.css(table_selector)[1].css('tr')
-
+        tables = response.css(table_selector)
         url = response.url
-        name = ''
-        title = ''
-        other_names = ''
-        birth_year = None
-        rule_start = None
-        rule_end = None
-        death_year = None
-        notable_for = ''
-        
+        main_table = self.get_main_table(tables)
+        table_rows = main_table.css('tr')
+
+        profile = {
+            'url': url,
+            'referrer': response.request.headers.get('Referer', None),
+            'name': '',
+            'other_names': '',
+            'titles': '',
+            'birth_year': None,
+            'rule_start': None,
+            'rule_end': None,
+            'death_year': None,
+            'notable_for': '',
+            'children': '',
+        }
+
         for idx, table_row in enumerate(table_rows):
-            # Get Name
+            td_list = []
+
             if(idx == 0):
-                name = table_row.css('th').get()
-            
+                profile['name'] = table_row.css('th::text').get()
+
             td_list = table_row.css('td')
-            pass
-            # Other Names
-            # Get Title (If any)
-            # Birth
-            # Rule
-            # Death 
-    
+
+            if(len(td_list) != 2):
+                continue
+
+            row_label = unicodedata.normalize(
+                'NFKC',
+                td_list.css('td:first-child::text').get() or 'default'
+            )
+
+            key = {
+                'Other names': 'other_names',
+                'Titles': 'titles',
+                'Birth': 'birth_year',
+                'Rule': 'rule_start',
+                'Death': 'death_year',
+                'Notable For': 'notable_for',
+                'Children': 'children',
+                'default': ''
+            }.get(row_label, '')
+
+            profile = self.extract_row_value(profile, td_list, key)
+
+        self.save_profile(profile)
+        for child_url in profile['children']:
+            yield Request(url=BASE_URL + child_url,
+                          callback=self.parse,
+                          meta={'depth': response.meta['depth'] + 1})
+
+    def extract_row_value(self, profile, td_list, key):
+        value_col = td_list.css('td:nth-child(2)')
+
+        if key == "other_names":
+            profile[key] = (
+                self.extract_other_names(value_col))
+
+        if key == "titles":
+            profile[key] = (
+                self.extract_titles(value_col))
+
+        if key == "birth_year":
+            profile[key] = (
+                self.extract_birth_year(value_col))
+
+        if key == "rule_start":
+            profile[key] = (
+                self.extract_rule_start(value_col))
+
+        if key == "death_year":
+            profile[key] = (
+                self.extract_death_year(value_col))
+
+        if key == "notable_for":
+            profile[key] = (
+                self.extract_notable_for(value_col))
+
+        if key == "children":
+            profile[key] = (
+                [child_link.attrib['href']
+                    for child_link in self.extract_children(value_col)])
+        return profile
+
+    def extract_other_names(self, selector):
+        return selector.css('::text').get()
+
+    def extract_titles(self, selector):
+        return selector.css('::text').get()
+
+    def extract_birth_year(self, selector):
+        birth_string = ''.join(selector.css('::text').getall())
+        return re.split(r"  ", birth_string)[0]
+
+    def extract_rule_start(self, selector):
+        birth_string = ''.join(selector.css('::text').getall())
+        return re.split(r"  ", birth_string)[0]
+
+    def extract_death_year(self, selector):
+        birth_string = ''.join(selector.css('::text').getall())
+        return re.split(r"  ", birth_string)[0]
+
+    def extract_notable_for(self, selector):
+        return selector.css('::text').get()
+
+    def extract_children(self, selector):
+        return selector.css('td>a')
+
     def get_element(self, selector, path):
         return selector.css(path)
 
-    def write_to_file(self, content_string):
-        with open(self.output_file, 'a') as fp:
-            fp.write(content_string)
-            fp.write('\n')
+    def save_profile(self, profile):
+        headers = [
+            'url',
+            'referrer',
+            'name',
+            'other_names',
+            'titles',
+            'birth_year',
+            'rule_start',
+            'rule_end',
+            'death_year',
+            'notable_for'
+        ]
+
+        to_save = {header: profile[header] for header in headers}
+
+        with open(self.output_file, 'a+') as fp:
+            dict_writer = DictWriter(fp, delimiter='|', fieldnames=headers)
+            dict_writer.writerow(to_save)
